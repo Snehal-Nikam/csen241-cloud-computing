c1e718bf93f07c579737bbf7393569725ce19924
Snehal_Nikam <snehalvnikam@gmail.com>
2024-01-29 14:24:34 -0800

    updated test results

--- home-work-1/sysbenchBenchmarkScript.sh
+++ home-work-1/sysbenchBenchmarkScript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 TestCases=(
-    #"cpu --cpu-max-prime= 1000"
-    #"cpu --cpu-max-prime= 1000"
-    #"memory --memory-block-size= 2K"
-    #"memory --memory-block-size= 4K"
+    #"cpu --cpu-max-prime=1000"
+    #"cpu --cpu-max-prime=1000"
+    #"memory --memory-block-size=2K"
+    #"memory --memory-block-size=4K"
     "fileio --file-test-mode=rndrw --file-total-size=3G"
     "fileio --file-test-mode=seqwr --file-total-size=3G"
     )
@@ -14,22 +14,23 @@ for testCase in "${TestCases[@]}"; do
     read -r mode parameter value <<<"$testCase"
     echo "Running sysbench with Mode: $mode, Parameter: $parameter, Value: $value"
     
-    filename="${mode}_${parameter}_${value// /}.text"
+    filename="${mode}_${parameter}_${value// /}.txt"
 
     if [[ $mode == "fileio" ]]; then
-        sysbench --test=fileio --file-total-size=3G --time=30 --file-test-mode=$value prepare
+        sysbench fileio --file-total-size=3G --time=30 $parameter prepare
     fi
 
     for ((i=1; i<=5; i++)); do
         echo "Run $i:" | tee -a "$filename"
-        results=$(sysbench $mode $parameter$value --time=30 run)
+        echo "Command: sysbench $mode $parameter $value run" >> "$filename"
+        results=$(sysbench $mode $parameter $value --time=30 run)
         echo "Results:" | tee -a "$filename"
         echo "$results" | tee -a "$filename"
         echo "----------------------------------------"| tee -a "$filename"
     done
 
     if [[ $mode == "fileio" ]]; then
-        sysbench --test=fileio --file-total-size=3G --time=30 --file-test-mode=$value cleanup
+        sysbench fileio --file-total-size=3G --time=30 $parameter cleanup
     fi
 
 done
